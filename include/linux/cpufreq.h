2f86c8fb5ed2016799117455ce87c4e07567b9f0
Krizthian <cristianortizmorales2013@gmail.com>
2015-11-29 18:06:23 -0500

    cpufreq: Introduce Impulse governor
    Impulse produce a balance in performance & battery.
    
    Signed-off-by: Krizthian <cristianortizmorales2013@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -385,6 +385,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
 extern struct cpufreq_governor cpufreq_gov_xperience;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_xperience)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
